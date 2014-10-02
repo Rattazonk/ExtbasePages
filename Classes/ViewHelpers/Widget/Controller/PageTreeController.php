a3ec3c8c6f91c698f09e071c0de8798db3dbd774
Frederik R. Vosberg <frederik.vosberg@rattazonk.com>
2014-10-02 13:36:58 +0200

    [FEATURE] clean up PageTreeController
    and initialize a pageTree for the logic

--- Classes/ViewHelpers/Widget/Controller/PageTreeController.php
+++ Classes/ViewHelpers/Widget/Controller/PageTreeController.php
@@ -15,93 +15,21 @@ class PageTreeController extends AbstractWidgetController {
 	protected $treeFilters = array();
 
 	/**
-	 * @var Rattazonk\Extbasepages\Domain\Repository\PageRepository
+	 * @var Rattazonk\Extbasepages\Tree\PageTree
 	 * @inject
 	 */
-	protected $pageRepository;
+	protected $pageTree;
 
 	public function indexAction() {
-		$this->initConfiguration();
 		$tree = $this->getTree();
-		
 		$this->view->assign('treeAlias', array($this->treeName => $tree) );
 	}
 
-	protected function initConfiguration() {
-		$this->startPid = $this->widgetConfiguration['underPid'];
-		$this->treeName = $this->widgetConfiguration['as'];
-	}
-
 	protected function getTree() {
-		$tree = $this->pageRepository->findByParent( $this->startPid );
-		$tree = $this->initWrappers( $tree );
-		$tree = $this->filterTree( $tree );
-
+		foreach( $this->widgetConfiguration as $name => $value ) {
+			$this->pageTree->addConfiguration($name, $value);
+		}
 		return $tree;
 	}
-
-	protected function filterTree( $tree, $init = TRUE ) {
-		if( $init ) {
-			$this->initTreeFilters();
-		}
-
-		foreach( $tree as $element ) {
-			foreach( $this->treeFilters as $filter ){
-				$filter->filter( $element );
-
-				if(	$this->widgetConfiguration['renderChildrenOfSkipped']
-					|| !$element->wrappedElementIsHidden() ) {
-
-					$element->setChildren( 
-						$this->filterTree( $element->getChildren(), FALSE )
-					);
-				}
-
-				if( $element->wrappedElementIsHidden() ) {
-					break;
-				}
-			}
-		}
-
-		return $tree;
-	}
-
-	protected function initTreeFilters() {
-		// should call addTreeFilter, if it is responsible
-		$this->signalSlotDispatcher->dispatch(__CLASS__, __FUNCTION__, array($this));
-	}
-
-	protected function initWrappers( $tree ) {
-		$wrappedTree = array();
-
-		foreach( $tree as $element ) {
-			$wrappedElement = $this->objectManager->get(
-				'Rattazonk\Extbasepages\Tree\ElementWrapper', 
-				$element
-			);
-			$wrappedChildren = $this->initWrappers(
-				$element->getChildren()
-			);
-
-			$wrappedElement->setChildren( $wrappedChildren );
-
-			$wrappedTree[] = $wrappedElement;
-		}
-
-		return $wrappedTree;
-	}
-
-	public function getWidgetConfiguration() {
-		return $this->widgetConfiguration;
-	}
-
-	/**
-	 * @param Rattazonk\Extbasepages\Tree\Filter\AbstractFilter $treeFilter
-	 * @return void
-	 */
-	public function addTreeFilter(AbstractTreeFilter $treeFilter) {
-		$this->treeFilters[] = $treeFilter;
-	}
-
 }
 ?>
